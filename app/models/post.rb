157e86ab0f1811668380781a73dcf4e981e3f552
Takayuki Kumagai <xtakayukikumagai@gmail.com>
2023-08-09 22:05:35 +0900

    add status column to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+  enum status: { writing: 1, public: 2 }
 end
---
+++ db/migrate/20230809130516_add_column_to_post.rb
@@ -0,0 +1,5 @@
+class AddColumnToPost < ActiveRecord::Migration[7.0]
+  def change
+    add_column :posts, :status, :integer
+  end
+end
